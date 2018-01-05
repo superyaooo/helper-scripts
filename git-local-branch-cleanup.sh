444ad8353ac656fc9c75571db785b7661de8b14a
superyaooo <superyaooo@gmail.com>
2018-01-05 16:31:09 -0600

    1st commit

---
+++ git-local-branch-cleanup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+decorator="***********"
+echo "$decorator Ni hao, $(whoami) $decorator"  # if $USER not working; can also be $(id -n -u)
+echo -e "$decorator Today is \c";date   # get date
+echo "You have the following files in the current directory, $PWD"
+ls   # list some effin files
+
+
+
+
+
+if [git ls-remote --heads ${REPO} ${BRANCH} | wc -l]     # check if remote branch exists - 1 = yes, 0 = no
+
+
+
+# list all local branches
+git branch
