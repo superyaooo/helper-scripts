46491b6309449b4eeb9b3fea00d2a43028f26134
superyaooo <superyaooo@gmail.com>
2018-01-25 14:09:03 -0600

    display no outdated branch found properly

--- git-local-branches-cleanup.sh
+++ git-local-branches-cleanup.sh
@@ -15,19 +15,25 @@ echo -en '\n'     # `e` interprets `\n`; `n` prevents empty/extra line on the en
 
 # remove outdated local branches
 echo "Searching for outdated local branches..."
+echo -en '\n'
 repo=$(git config --get remote.origin.url)    # back tick works too; `git config --get remote.origin.url` gets repo url
+outdatedBranchCounter=0
+
 for x in $branches
 do
   remoteExists=$(git ls-remote --heads $repo $x | wc -l)   # check if remote branch exists - 1 = yes, 0 = no
   if [ $remoteExists = 0 ]; then    # space after & before square brackets is needed!!!
     echo "Outdated local branch found: $x"
+    ((outdatedBranchCounter++))    # counter + 1
     deleteBranch=$(git branch -d "$x" | wc -l)
     if [ $deleteBranch = 1 ]; then
       echo ":) local branch $x deleted successfully"
     else
       echo "!! error occurred while deleting local branch $x"
     fi
-  else
-    echo ":) no outdated branch found"        # TODO - need to NOT echo this when branh is found and deleted
   fi
 done
+
+if [ $outdatedBranchCounter = 0  ]; then
+  echo ":) no outdated branch found"
+fi
